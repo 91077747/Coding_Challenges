c23fe423e840adb2d72a07f667841058c501467f
91077747 <91077747@mvusd.net>
2017-09-20 08:07:36 -0700

    added challanges to Ch001

--- src/Ch001.java
+++ src/Ch001.java
@@ -1,3 +1,9 @@
+/*
+ * Author: Victoria Haan
+ * Created: 9/20/17
+ * Description:Ch001 - Print out the words, “Hello, world!” to the screen.
+ * 
+ */
 public class Ch001
 {
   public static void main(String[] args)
